a84b33aabe2a49874027db07a02acd3dc603710d
Alexandre Nunes <alexandrenunes1331@gmail.com>
2016-01-14 21:12:25 +0000

    City Chnages and score was advertising

---
+++ SCRIPTS/getPUB.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var PUBScreen:GameObject;
+
+function Start () {
+	var PUB=new WWW("http://"+PlayerPrefs.GetString("SYSTEM_IP")+"/pub.jpg");
+	yield PUB;
+	if(PUB.error==null)
+		if(!PUB.text.Contains("Error 404"))
+			PUBScreen.GetComponent.<Renderer>().material.mainTexture=PUB.texture;
+}
